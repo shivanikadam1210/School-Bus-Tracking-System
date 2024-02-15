e4f0ac4b38b22577174c520bc96ae1efc83096db
shivanikadam1210 <160107567+shivanikadam1210@users.noreply.github.com>
2024-02-15 13:08:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ const body = document.querySelector("body"),
+sidebar = body.querySelector(".sidebar"),
+toggle=body.querySelector(".toggle"),
+searchBtn= body.querySelector(".search-box");
+modeSwtich=body.querySelector(".toggle-switch"),
+// modeText = body.querySelector("mode-text");
+
+toggle.addEventListener("click",() =>{
+// searchBtn.addEventListener("click", () =>{
+    
+    sidebar.classList.toggle("close");
+   }); 
+modeSwtich.addEventListener("click", () =>{
+    // body.classList.toggle("dark");
+
+});
